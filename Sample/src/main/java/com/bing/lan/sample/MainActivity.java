4000da12ccd1b90b951693d25d0d06c3a4b66cc0
lanbing <lanbing@baogongyoucai.com>
2018-03-19 17:57:17 +0800

    remove jni code

--- Sample/src/main/java/com/bing/lan/sample/MainActivity.java
+++ Sample/src/main/java/com/bing/lan/sample/MainActivity.java
@@ -14,9 +14,9 @@ import android.view.MenuItem;
 public class MainActivity extends AppCompatActivity {
 
     // Used to load the 'native-lib' library on application startup.
-    static {
-        System.loadLibrary("native-lib");
-    }
+    //static {
+    //    System.loadLibrary("native-lib");
+    //}
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,7 +36,7 @@ public class MainActivity extends AppCompatActivity {
 
         // Example of a call to a native method
         TextView tv = (TextView) findViewById(R.id.sample_text);
-        tv.setText(stringFromJNI());
+        //tv.setText(stringFromJNI());
 
     }
 
@@ -66,5 +66,5 @@ public class MainActivity extends AppCompatActivity {
      * A native method that is implemented by the 'native-lib' native library,
      * which is packaged with this application.
      */
-    public native String stringFromJNI();
+    //public native String stringFromJNI();
 }
